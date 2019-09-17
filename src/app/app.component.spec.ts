a630e575e4cf5c4ccbb51a79cd4a819fd2f8edf2
William Sedlacek <wsedlacekc@gmail.com>
2019-09-17 13:10:20 -0500

    Correct spelling

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,10 +14,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'portofolio'`, () => {
+  it(`should have as title 'portfolio'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('portofolio');
+    expect(app.title).toEqual('portfolio');
   });
 
   it('should render title', () => {
@@ -25,7 +25,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('.content span').textContent).toContain(
-      'portofolio app is running!'
+      'portfolio app is running!'
     );
   });
 });
