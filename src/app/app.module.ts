a25d284f4da4162bfe2f1e6f338ae2a6aaafc7c7
William Sedlacek <wsedlacekc@gmail.com>
2020-04-19 12:36:13 -0500

    feature: Akita
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
+
+import { environment } from '../environments/environment';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    environment.production ? [] : AkitaNgDevtools,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
