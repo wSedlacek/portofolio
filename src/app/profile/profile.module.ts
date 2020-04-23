4c6bbf1983f1f2720f80ccb0e1127403ea199292
William Sedlacek <wsedlacekc@gmail.com>
2020-04-23 09:50:10 -0500

    style: Profile Styling

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -1,8 +1,22 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule } from '@angular/material/button';
+
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+import {
+  FaIconLibrary,
+  FontAwesomeModule,
+} from '@fortawesome/angular-fontawesome';
+import {
+  faGitlab,
+  faLinkedin,
+  faTwitter,
+} from '@fortawesome/free-brands-svg-icons';
 
 import { ProfileComponent } from './profile.component';
 import { ProfileQuery } from './state/profile.query';
@@ -11,8 +25,20 @@ import { ProfileStore } from './state/profile.store';
 
 @NgModule({
   declarations: [ProfileComponent],
-  imports: [CommonModule, MatCardModule, MatProgressSpinnerModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+    FlexLayoutModule,
+    FontAwesomeModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   providers: [ProfileQuery, ProfileService, ProfileStore],
   exports: [ProfileComponent],
 })
-export class ProfileModule {}
+export class ProfileModule {
+  constructor(private readonly library: FaIconLibrary) {
+    library.addIcons(faTwitter, faGitlab, faLinkedin);
+  }
+}
