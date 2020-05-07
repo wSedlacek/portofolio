23e74f9982c219b7a9715ecbc5b1a03839b63329
William Sedlacek <wsedlacekc@gmail.com>
2020-05-07 00:02:50 -0500

    fix: Better loading styles

--- src/app/features/about/about.module.ts
+++ src/app/features/about/about.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
 import { MatChipsModule } from '@angular/material/chips';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { AboutComponent } from './about.component';
 import { AboutQuery } from './state/about.query';
@@ -9,7 +10,7 @@ import { AboutService } from './state/about.service';
 import { AboutStore } from './state/about.store';
 
 @NgModule({
-  imports: [CommonModule, MatChipsModule],
+  imports: [CommonModule, MatChipsModule, MatProgressSpinnerModule],
   declarations: [AboutComponent],
   providers: [AboutQuery, AboutService, AboutStore],
   exports: [AboutComponent],
