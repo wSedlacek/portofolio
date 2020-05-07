73a68206de61e1fdf3b06fdc6829c34c687ca1e4
William Sedlacek <wsedlacekc@gmail.com>
2020-05-06 19:27:02 -0500

    fix: GitLab endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  gitlabAPI: 'https://gitlab.com/api/v4/',
+  gitlabAPI: 'https://gitlab.com/api/v4',
   userId: '4406562',
   contentful: {
     space: 'i3zt395gkmh6',
